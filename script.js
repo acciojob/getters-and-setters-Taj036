d7654fbd185d3037b7c6dc4dc0dc1727fd0ba66b
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-15 00:54:36 +0000

    created a submission

--- script.js
+++ script.js
@@ -36,6 +36,19 @@ class Teacher extends Person {
         console.log(`${this.name} is teaching.`);
     }
 }
+const person1 = new Person("John", 30);
+console.log(person1.getName); // Output: My name is John
+console.log(person1.getAge);  // Output: 30
+
+const student1 = new Student("Alice", 20);
+student1.study(); // Output: Alice is studying.
+console.log(student1.getName); // Output: My name is Alice
+console.log(student1.getAge);  // Output: 20
+
+const teacher1 = new Teacher("Mr. Smith", 45);
+teacher1.teach(); // Output: Mr. Smith is teaching.
+console.log(teacher1.getName); // Output: My name is Mr. Smith
+console.log(teacher1.getAge); 
 
 // Do not change the code below this line
 window.Person = Person;
